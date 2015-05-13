cce60cdbaa8a37195dd63631a372914e19b4d4a1
intchloe <chloe@cmail.nu>
2015-05-13 18:38:55 +0200

    Update webrtc.bat

--- webrtc.bat
+++ webrtc.bat
@@ -1,2 +1,7 @@
-NETSH advfirewall firewall add rule name="WebRTC-block" dir="out" program="%ProgramFiles% (x86)\Google\Chrome\Application\chrome.exe" protocol="UDP" action="block" enable="yes"
+IF NOT EXIST "%ProgramFiles%\Google\Chrome\Application\" (
+	NETSH advfirewall firewall add rule name="WebRTC-block" dir="out" program="%ProgramFiles% (x86)\Google\Chrome\Application\chrome.exe" protocol="UDP" action="block" enable="yes"
+	echo DONE!
+	) ELSE (
 NETSH advfirewall firewall add rule name="WebRTC-block" dir="out" program="%ProgramFiles%\Google\Chrome\Application\chrome.exe" protocol="UDP" action="block" enable="yes"
+echo DONE!
+)
