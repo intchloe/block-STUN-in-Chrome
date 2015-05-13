35c2e05ad03eff8b5a7138a0f9dd2b51ade177af
intchloe <chloe@cmail.nu>
2015-05-13 18:40:25 +0200

    Update webrtc.bat

--- webrtc.bat
+++ webrtc.bat
@@ -1,7 +1,7 @@
 IF NOT EXIST "%ProgramFiles%\Google\Chrome\Application\" (
 	NETSH advfirewall firewall add rule name="WebRTC-block" dir="out" program="%ProgramFiles% (x86)\Google\Chrome\Application\chrome.exe" protocol="UDP" action="block" enable="yes"
-	echo DONE!
+	start chrome "https://diafygi.github.io/webrtc-ips/"
 	) ELSE (
 NETSH advfirewall firewall add rule name="WebRTC-block" dir="out" program="%ProgramFiles%\Google\Chrome\Application\chrome.exe" protocol="UDP" action="block" enable="yes"
-echo DONE!
+start chrome "https://diafygi.github.io/webrtc-ips/"
 )
